603c67a06b66c60740677599686d80d194a05ecc
Fikri Maulana <h47ch1@gmail.com>
2017-08-24 16:41:16 +0700

    Create bit.config.php

---
+++ bit.config.php
@@ -0,0 +1,8 @@
+<?php
+   $user = "..."; your Bitsler username
+   $token = "..."; SHA512. Go to www.bitsler.com & login, in console browser (PRESS F12) type: console.log(access_token)
+   #$persen = "...."; example "80.00" for dice mode remove #
+   #$pilihan = "true-true-true-false"; for CaraYCruz remove #
+   #$api = "http://smaalazhaar.sch.id/modul/bd.php"; for dice mode remove #
+   #$api = http://smaalazhaar.sch.id/modul/bs.php"; for CaraYCruz remove #
+?>
