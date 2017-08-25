8dfadc93261123d62c40409173b16d716b5fecc7
Fikri Maulana <h47ch1@gmail.com>
2017-08-25 13:23:34 +0700

    Update bit.config.php

--- bit.config.php
+++ bit.config.php
@@ -1,6 +1,6 @@
 <?php
-   #$user = "..."; //your Bitsler username
-   #$token = "..."; //SHA512. Go to www.bitsler.com & login, in console browser (PRESS F12) type: console.log(access_token)
+   $user = "..."; //your Bitsler username
+   $token = "..."; //SHA512. Go to www.bitsler.com & login, in console browser (PRESS F12) type: console.log(access_token)
    #$pilihan = "80.00"; //example "80.00" for dice mode remove #
    #$pilihan = "true-true-true-false"; //for CaraYCruz remove #
    #$api = "http://smaalazhaar.sch.id/modul/bd.php"; //for dice mode remove #
